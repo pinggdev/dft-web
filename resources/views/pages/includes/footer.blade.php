c0c24ba171db2169d3709a27689c9809293ca511
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-05-02 17:25:40 +0700

    adding bootstrap and google font

---
+++ resources/views/pages/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ asset('assets/js/bootstrap.js') }}"></script>
+</body>
+</html>
---
+++ resources/views/pages/master.blade.php
@@ -0,0 +1,5 @@
+@include('pages.includes.header')
+
+<h1>tes</h1>
+
+@include('pages.includes.footer')
