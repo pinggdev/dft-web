3faae9850034d9acf519ecb79f652e9296727e52
Kevin Sinaga <kevinsinaga11.ks@gmail.com>
2023-05-10 20:16:37 +0700

    adding layanan page

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,7 @@ Route::get('/', function () {
 Route::get('/tentang', function () {
     return view('pages.tentang');
 });
+
+Route::get('/layanan', function () {
+    return view('pages.layanan');
+});
